3991c35a11713465ba9781a5edb291b4698d106f
Your Name <you@example.com>
2018-01-07 18:35:59 +0800

    新增文件

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 import unittest
+import logging
+from lib.Logger import Logger
+import traceback
 from lib import HTMLTestRunner
 import time
 from testcase.case_login_logout.admin_login_logout import Bugfree管理员登录退出
 
-
 if __name__ == '__main__':
-    suite = unittest.TestSuite()     # 新建一个suite，测试套件
-    loader = unittest.TestLoader()   # 新建一个加载器，自定义的方式把测试用例加载到suite里
-    suite.addTests(loader.loadTestsFromTestCase(Bugfree管理员登录退出))  # 把测试类所有的方法都加载到suite里
-    #unittest.TextTestRunner(verbosity=2).run(suite) # unittest运行suite
-    fp = open('reports/report_bugfree_{0}.html'.format(time.strftime("%Y-%m-%d %H-%M-%S")), 'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(
-        stream=fp,
-        title='Bugfree的测试报告',
-        description='Bugfree的所有测试用例执行细节'
-    )
-    runner.run(suite)
-    fp.close()
-
+        logger = Logger('./log/logger.log', logging.INFO)
+        logging.info("本次测试开始执行，以下是详细日志")
+        try:
+            suite = unittest.TestSuite()  # 新建一个suite，测试套件
+            loader = unittest.TestLoader()  # 新建一个加载器，自定义的方式把测试用例加载到suite里
+            suite.addTests(loader.loadTestsFromTestCase(Bugfree管理员登录退出))  # 把测试类所有的方法都加载到suite里
+            # unittest.TextTestRunner(verbosity=2).run(suite) # unittest运行suite
+            fp = open('reports/report_bugfree_{0}.html'.format(time.strftime("%Y-%m-%d %H-%M-%S")), 'wb')
+            runner = HTMLTestRunner.HTMLTestRunner(
+                stream=fp,
+                title='Bugfree的测试报告',
+                description='Bugfree的所有测试用例执行细节'
+            )
+            runner.run(suite)
+            logging.info("测试顺利结束^_^ ")
+        except Exception:
+            """print_exc() 把异常输出到屏幕上，而format_exc() 把异常返回成字符串"""
+            traceback.print_exc()
+            logging.error(traceback.format_exc())
+            logging.error("测试异常终止")
+        finally:
+            if fp is not None:
+                fp.close()
--- testcase/case_login_logout/admin_login_logout.py
+++ testcase/case_login_logout/admin_login_logout.py
@@ -3,29 +3,34 @@ import unittest
 import time
 from selenium import webdriver
 import ddt
+import logging
+from lib.utils import capture_screen
 
 test_data = [['admin', '123456', '退出'],
              ['invalid', '123456', '用户名不存在'],
-             ['', '123456', '不可为空白'],
-             ['sss', '123456', '用户名不存在']]
+             ['', '123456', '不可为空白']]
+
 @ddt.ddt
 class Bugfree管理员登录退出(unittest.TestCase):
     """
     演示的是bugfree的登录和退出
+    数据驱动，相同的逻辑使用不同的数据去运行。
     """
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://192.168.2.87"
+        self.base_url = "http://localhost"
         driver = self.driver
-
+        logging.info("打开浏览器成功")
 
     def tearDown(self):
         pass
 
     @ddt.unpack
     @ddt.data(*test_data)
-    def test_admin_login(self, admin, password, flag):
+    def test_admin_login_test(self, admin, password, flag):
+        """admin的登录的所有测试用例"""
+        logging.info("test_admin_login_test start....")
         driver = self.driver
         driver.get(self.base_url + "/bugfree/index.php/site/login")
         driver.find_element_by_id("LoginForm_username").clear()
@@ -35,7 +40,14 @@ class Bugfree管理员登录退出(unittest.TestCase):
         driver.find_element_by_id("LoginForm_rememberMe").click()
         driver.find_element_by_id("SubmitLoginBTN").click()
         time.sleep(3)
-        self.assertIn(flag,driver.page_source)
+        self.assertIn(flag, driver.page_source)
+        logging.info("test data is : {}, {}, {}".format(admin, password, flag))
+        pic_path = capture_screen(driver)
+        if pic_path is None:
+            logging.error("截图不成功")
+        else:
+            logging.info(pic_path)
+        logging.info("test_admin_login_test end....")
 
 
 if __name__ == '__main__':
