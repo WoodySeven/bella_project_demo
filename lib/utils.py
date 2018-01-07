a1bd8fcb78fd247ffbf97d622d7a7d33dae51279
Your Name <you@example.com>
2018-01-07 15:30:47 +0800

    修改文件“

---
+++ lib/utils.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import  shutil
+import os
+
+shutil.copyfile("HTMLTestRunner.py", "c:\HTMLTestRunner.py")
+os.system("del c:\\HTMLTestRunner.py")
--- testcase/case_login_logout/admin_login_logout.py
+++ testcase/case_login_logout/admin_login_logout.py
@@ -2,31 +2,40 @@
 import unittest
 import time
 from selenium import webdriver
+import ddt
 
-
-class BugfreeAdminLoginLogout(unittest.TestCase):
+test_data = [['admin', '123456', '退出'],
+             ['invalid', '123456', '用户名不存在'],
+             ['', '123456', '不可为空白'],
+             ['sss', '123456', '用户名不存在']]
+@ddt.ddt
+class Bugfree管理员登录退出(unittest.TestCase):
     """
     演示的是bugfree的登录和退出
     """
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://localhost"
+        self.base_url = "http://192.168.2.87"
         driver = self.driver
+
 
     def tearDown(self):
         pass
 
-    def test_admin_login(self):
+    @ddt.unpack
+    @ddt.data(*test_data)
+    def test_admin_login(self, admin, password, flag):
         driver = self.driver
         driver.get(self.base_url + "/bugfree/index.php/site/login")
         driver.find_element_by_id("LoginForm_username").clear()
-        driver.find_element_by_id("LoginForm_username").send_keys("admin")
+        driver.find_element_by_id("LoginForm_username").send_keys(admin)
         driver.find_element_by_id("LoginForm_password").clear()
-        driver.find_element_by_id("LoginForm_password").send_keys("123456")
+        driver.find_element_by_id("LoginForm_password").send_keys(password)
         driver.find_element_by_id("LoginForm_rememberMe").click()
         driver.find_element_by_id("SubmitLoginBTN").click()
         time.sleep(3)
+        self.assertIn(flag,driver.page_source)
 
 
 if __name__ == '__main__':
